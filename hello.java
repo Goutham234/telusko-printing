6bb2e51cb8ea3d49e52243d7a1c847fd0031e725
Goutham234 <100072904+Goutham234@users.noreply.github.com>
2022-06-23 12:12:16 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,68 @@
+class hello {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 17; i++) {
+            if (i == 4 || i == 7 || i == 9 || i == 10 || i == 13 || i == 14 || i == 15)
+                System.out.print("  ");
+            if (i == 8)
+                System.out.print("   ");
+
+            System.out.print("*");
+
+
+        }
+        System.out.println("");
+        for (int i = 1; i <= 10; i++) {
+            if (i == 1  || i==8 || i==10)
+                System.out.print(" ");
+            if (i == 5 || i == 6)
+                System.out.print("  ");
+            if (i == 2 || i == 4 || i==9)
+                System.out.print("   ");
+            if (i == 3 || i == 7)
+                System.out.print("    ");
+
+            System.out.print("*");
+        }
+        System.out.println("");
+
+
+        for (int i = 1; i <= 13; i++) {
+            if (i == 1 || i == 13)
+                System.out.print(" ");
+            if (i == 2 || i == 6)
+                System.out.print("   ");
+            if (i == 5 || i == 7 || i == 8 || i == 11)
+                System.out.print("  ");
+            if (i == 12)
+                System.out.print("     ");
+            System.out.print("*");
+        }
+        System.out.println("");
+        for (int i = 1; i <= 10; i++) {
+            if (i == 1 || i == 8 || i==10)
+                System.out.print(" ");
+            if (i == 5 || i==7)
+                System.out.print("  ");
+            if (i == 2 || i == 4 || i==9)
+                System.out.print("   ");
+            if (i == 3 || i==6 )
+                System.out.print("    ");
+            System.out.print("*");
+        }
+        System.out.println("");
+        for (int i = 1; i <= 19; i++) {
+            if(i==1 || i==8)
+                System.out.print(" ");
+            if(i==5 || i==12 || i==15 || i==16 || i==17)
+                System.out.print("  ");
+            if(i==2)
+                System.out.print("   ");
+
+            System.out.print("*");
+        }
+
+
+        }
+}
+
+
